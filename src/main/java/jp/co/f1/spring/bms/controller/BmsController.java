ffb65d2753c6a4447ac14fc928dbee5e9696de51
reisa <reisa@192.168.11.16>
2022-06-15 00:13:56 +0900

    "アンマリ変えてない"

--- src/main/java/jp/co/f1/spring/bms/controller/BmsController.java
+++ src/main/java/jp/co/f1/spring/bms/controller/BmsController.java
@@ -1,10 +1,14 @@
 package jp.co.f1.spring.bms.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import jp.co.f1.spring.bms.entity.Book;
@@ -13,8 +17,53 @@ import jp.co.f1.spring.bms.repository.BookRepository;
 @Controller
 public class BmsController {
 	
+	
+	
 	@Autowired
 	private BookRepository bookinfo;
+	
+	
+	
+	
+	
+	
+	@RequestMapping("/insert")
+	public ModelAndView insert(ModelAndView mav) {
+		
+		mav.setViewName("insert");
+		
+		return mav;
+		
+	}
+	
+//	insertにアクセスがあった場合
+	
+	@RequestMapping(value= "/insert", method=RequestMethod.POST)
+	
+	
+//	@ModelAttributeあのテーション
+	//postmedhotで送信されたデータをBookインスタンスで受け取る。
+	//Book book = new book として1つずつセットしてもいい。
+	
+	public ModelAndView insertPost(@ModelAttribute Book book, ModelAndView mav) {
+		
+//		bookinfoというでーたベースにbookインスタンスで送られてきたデータを自動で
+//		保存してくれる
+		bookinfo.saveAndFlush(book);
+
+//		forwardの場合もforward:と記述する。
+		
+		
+		
+		mav = new ModelAndView("redirect:/list");
+		
+		return mav;
+		
+	}
+	
+//	insertに対してぽスト送信された場合
+	
+//	@Repository
 	
 	@RequestMapping("/list")
 	public ModelAndView list(ModelAndView mav) {
@@ -33,36 +82,35 @@ public class BmsController {
 		
 	}
 	
-//	insertにアクセスがあった場合
+//	@RequestMapping(value= "/detail")
+//	public ModelAndView detail(@ModelAttribute Book book, ModelAndView mav) {
+//		
+//		Optional<Book> book1 = bookinfo.findByIsbn(book.getIsbn());
+//		
+////		取得したエンティティクラス（viewから送られてきた値をもとに
+////		bookインスタンスを取得する）
+//		
+////		取得したエンティティをviewに渡す
+//		
+//		mav.addObject("book",book1.get());
+//		
+//		mav.setViewName("detail");
+//		
+//		
+//		return mav;
+//	}
 	
-	@RequestMapping("/insert")
-	public ModelAndView insert(ModelAndView mav) {
+	
+	@GetMapping("/detail")
+	public ModelAndView detail(@RequestParam("isbn") String isbn, ModelAndView mav) {
 		
-		mav.setViewName("insert");
+		Optional<Book> book1 = bookinfo.findByIsbn(isbn);
+		
+		mav.addObject("book",book1.get());
+		mav.setViewName("detail");
 		
 		return mav;
-		
 	}
-	
-//	insertに対してぽスト送信された場合
-	
-	@RequestMapping(value= "/insert", method=RequestMethod.POST)
-	
-	
-//	@ModelAttributeあのテーション　　
-	public ModelAndView insertPost(@ModelAttribute Book book, ModelAndView mav) {
-		
-		bookinfo.saveAndFlush(book);
-
-		mav = new ModelAndView("redirect:/list");
-		
-		return mav;
-		
-	}
-	
-	
-	
-	
 	
 	
 }
--- src/main/java/jp/co/f1/spring/bms/repository/BookRepository.java
+++ src/main/java/jp/co/f1/spring/bms/repository/BookRepository.java
@@ -1,14 +1,36 @@
 package jp.co.f1.spring.bms.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import jp.co.f1.spring.bms.entity.Book;
 
 //おそらくrailsでいうところのアクティブレコードのようなものこれでメソッドを色々使うことができる
-
+//jpa以外にもmybatis・hibernateが用意システムごとに使い分けられる。
+//基本的にEntity毎にRepositoryを作成します。
 @Repository
 public interface BookRepository extends JpaRepository<Book,String> {   //DTOクラスと型
 	
+	
+	//findByIsbnメソッドisbn番号を引数にしてBookインスタンスを取り出すメソッド
+//	Bookクラスから自動で検索してきてくれる。
+//	findByIsbn
+//	↓
+//	"find" "By" "Isbn"(引数)
+//	↓
+//	select * from テーブル　where isbn = 引数；
+//	
+	
+	
+	
+	
+//	しかし戻り値がOptionalとなってるnullでも何とかしてくれる
+	
+	
+	
+	
+	public Optional<Book> findByIsbn(String isbn);
 
 }
